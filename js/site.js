8088ada19825170f4043eae2c5bbd53e38311ec2
Joel Marques <bitmarques@gmail.com>
2017-02-15 21:45:13 -0200

    alteracao no carousel

--- js/site.js
+++ js/site.js
@@ -10,11 +10,26 @@ angular.module('site', ['ui.bootstrap'])
 
 	vm.visitante = {};
 
+	//carousel pesquisadores
+	vm.pesquisadores = [];
+	vm.pQuantidadePorSlide = 3;
+	vm.pInicio = 1;
+	vm.pFim = vm.pQuantidadePorSlide;
+
+	//carousel animais
+	vm.animais = [];
+	vm.aQuantidadePorSlide = 6;
+	vm.aInicio = 1;
+	vm.aFim = vm.aQuantidadePorSlide;
+
 	$http.get(variaveisGlobais.ambiente + 'publico/sites?identificador=' + variaveisGlobais.identificador).success(function(site) {
 		vm.site = site;
 		vm.visitante.identificadorDoSite = variaveisGlobais.identificador;
 		vm.visitante.emailDoSite = vm.site.empresa.email;
 		vm.visitante.descricaoDoSite = variaveisGlobais.descricaoDoSite;
+
+		vm.setPesquisadores();
+		vm.setAnimais();
 	});
 
 	vm.enviarMensagem = function() {
@@ -48,6 +63,66 @@ angular.module('site', ['ui.bootstrap'])
 			   vm.visitante.email != null &&
 			   vm.visitante.mensagem != null &&
 			   vm.visitante.emailDoSite != null;
+	}
+
+	// carousel pesquisadores
+	vm.setPesquisadores = function() {
+
+		vm.pesquisadores = [];
+
+		vm.pFim = vm.pInicio + (vm.pQuantidadePorSlide - 1);
+
+		if (vm.pFim > vm.site.pesquisadores.length) {
+			vm.pFim = vm.site.pesquisadores.length;
+		}
+
+		for (var i=vm.pInicio; i<=vm.pFim; i++) {
+			vm.pesquisadores.push(vm.site.pesquisadores[i-1]);
+		}
+	}
+	
+	vm.pPrev = function() {
+		if (vm.pInicio > 1) {
+			vm.pInicio = vm.pInicio - vm.pQuantidadePorSlide;
+			vm.setPesquisadores()
+		}
+	}
+
+	vm.pNext = function() {
+		if (vm.pFim + 1 <= vm.site.pesquisadores.length) {
+			vm.pInicio = vm.pFim + 1;
+			vm.setPesquisadores()
+		}
+	}
+
+	// carousel animais
+	vm.setAnimais = function() {
+
+		vm.animais = [];
+
+		vm.aFim = vm.aInicio + (vm.aQuantidadePorSlide - 1);
+
+		if (vm.aFim > vm.site.animais.length) {
+			vm.aFim = vm.site.animais.length;
+		}
+
+		for (var i=vm.aInicio; i<=vm.aFim; i++) {
+			vm.animais.push(vm.site.animais[i-1]);
+		}
+	}
+
+	vm.aPrev = function() {
+		if (vm.aInicio > 1) {
+			vm.aInicio = vm.aInicio - vm.aQuantidadePorSlide;
+			vm.setAnimais()
+		}
+	}
+
+	vm.aNext = function() {
+		if (vm.aFim + 1 <= vm.site.animais.length) {
+			vm.aInicio = vm.aFim + 1;
+			vm.setAnimais()
+		}
 	}
 
 })
